02b09ec96f4b060b3ea404e1cf5fef8c5f9cdcf0
Thomas Munzer <thomas.munzer@gmail.com>
2019-10-02 09:57:31 +0200

    add secret file to store credentials

---
+++ secret.py
@@ -0,0 +1,20 @@
+""" 
+file to store the account credenitals or API token. 
+credentials variable may have two different format:
+-------
+1. Login/password
+
+credentials = {
+    "email": "user@domain"
+    "password": "xxxxx"
+    "host": 'api.mist.com'
+}
+
+-------
+2. API Token
+
+credentials = {
+    "apitoken" : "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "host": "api.mist.com"
+} 
+"""
