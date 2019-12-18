d4c982faa058e813c8f70cc059f81158c7b19dda
Thomas Munzer <thomas.munzer@gmail.com>
2019-12-18 14:13:51 +0100

    minor changes

--- org_admins_import.py
+++ org_admins_import.py
@@ -9,21 +9,23 @@ The script has 3 different steps:
 2) select the organisation where you want to add/invite the admins
 3) choose if you want to give access to the whole organisation, or only to specific sites
 '''
+#### PARAMETERS #####
+csv_separator = ","
+privileges = []
+
+#### IMPORTS ####
 import mlib as mist_lib
 from tabulate import tabulate
 import mlib.cli as cli
 import sys
 import csv
-#### PARAMETERS #####
-csv_separator = ","
-privileges = []
-    
-mist = mist_lib.Mist_Session()
 
+#### CONSTANTS ####
 roles = {"s": "admin", "n": "write", "o": "read", "h":"helpdesk"}
-role = ""
-if privileges == []:
-    org_id = cli.select_org(mist)
+
+#### FUNCTIONS ####
+def define_privileges(org_id):
+    role = ""
     while role not in roles:
         role = input("Which level of privilege at the org level (\"s\" for Super Admin, \"n\" for Network Admin,\"o\" for observer,\"h\" for helpdesk)")    
     while True:
@@ -35,25 +37,38 @@ if privileges == []:
             break
         elif all_sites.lower() == "n" or all_sites == "":            
             site_ids = mist_lib.org.sites.get(mist, org_id)
-            privileges.append({"scope": "org", "org_id": org_id, "site_id":site_ids["result"][0], "role":roles[role]})
-            break
+            site_id=""
+            for site in site_ids["result"]:
+                if "site_id" in site:
+                    site_id = site["site_id"]
+                    break
+            privileges.append({"scope": "org", "org_id": org_id, "site_id":site_id, "role":roles[role]})
+            break 
 
+def import_admins(org_id):
+    print("Opening CSV file %s" % sys.argv[1])
+    try:
+        with open(sys.argv[1], 'r') as my_file:
+            invite_file = csv.reader(my_file, delimiter=',')
+            for row in invite_file:  
+                email= row[0]
+                first_name= row[1]
+                last_name = row[2]        
+                print(', '.join(row))
+                mist_lib.requests.org.admins.create_invite(mist, org_id, email, privileges, first_name, last_name)            
+    except:
+        print("Error while opening the CSV file... Aborting")
 
+#### SCRIPT ENTRYPOINT ####
 
-print("Opening CSV file %s" % sys.argv[1])
-try:
-    with open(sys.argv[1], 'r') as my_file:
-        invite_file = csv.reader(my_file, delimiter=',')
-        for row in invite_file:  
-            email= row[0]
-            first_name= row[1]
-            last_name = row[2]        
-            print(', '.join(row))
-            mist_lib.requests.org.admins.create_invite(mist, org_id, email, privileges, first_name, last_name)            
-except:
-    print("Error while opening the CSV file... Aborting")
+mist = mist_lib.Mist_Session("./session.py")
+
+org_id = cli.select_org(mist)
+
+if privileges == []:
+    define_privileges(org_id)
+import_admins(org_id)
 
 admins = mist_lib.requests.org.admins.get(mist, org_id)['result']
 print(tabulate(admins))
-#print(admins)
 exit(0)
