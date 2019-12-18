5320c62033c599fb51f48660eca20c74a30cd6b6
Thomas Munzer <thomas.munzer@gmail.com>
2019-12-18 14:20:32 +0100

    update config_exmaple

--- config_example.py
+++ config_example.py
@@ -1,16 +1,23 @@
 """ 
-example of the "secret.py" file where you can store the
+/!\ OPTIONAL!!! /!\
+example of the "config.py" file where you can store the
 account credenitals or API token. 
 credentials variable may have two different format:
 -------
 1. Login/password
 
 credentials = {
-    "email": "user@domain"
-    "password": "xxxxx"
+    "email": "user@domain",
+    "password": "xxxxx",
     "host": 'api.mist.com'
 }
 
+OR
+
+credentials = {
+    "email": "user@domain",
+    "host": 'api.mist.com'
+}
 -------
 2. API Token
 
