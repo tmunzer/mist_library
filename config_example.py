610fc8d36d038ff21079de1eb079780d7893251b
Thomas Munzer <thomas.munzer@gmail.com>
2020-01-08 10:26:25 +0100

    update config examples

--- config_example.py
+++ config_example.py
@@ -1,5 +1,6 @@
-""" 
-/!\ OPTIONAL!!! /!\
+'''
+~~~ OPTIONAL ~~~~
+
 example of the "config.py" file where you can store the
 account credenitals or API token. 
 credentials variable may have two different format:
@@ -25,4 +26,23 @@ credentials = {
     "apitoken" : "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     "host": "api.mist.com"
 } 
-"""
+-------
+OTHER
+Log level: you can define the log level displayed on the console with the following variable (default 
+is 6):
+log_level = 6
+
+------ 
+COMPLETE EXAMPLE
+
+INFO: Be sure to uncomment the information to be used in your file!
+
+The information in you config.py file should look like (of course, it depends on the authentication 
+method you chose):
+
+credentials = {
+    "apitoken" : "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "host": "api.mist.com"
+} 
+log_level = 6
+'''
