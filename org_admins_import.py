3432eaf6fd2ab88a7cf46a6b0cc74ac72bf3b30e
Thomas Munzer <thomas.munzer@gmail.com>
2020-01-08 10:28:34 +0100

    improve comments

--- org_admins_import.py
+++ org_admins_import.py
@@ -1,6 +1,6 @@
 '''
 Python script to invite/add adminsitrators from a CSV file.
-The CSV file must have 3 rows: email, first name, last name
+The CSV file must have 3 columns: email, first name, last name
 
 You can run the script with the command "python3 org_admins_import.py <path_to_the_csv_file>"
 
@@ -25,6 +25,11 @@ roles = {"s": "admin", "n": "write", "o": "read", "h":"helpdesk"}
 
 #### FUNCTIONS ####
 def define_privileges(org_id):
+    '''
+    Generate the privilege parameters for the specified org.
+    Will ask if the privileges have to be applied to the entire org or to a specific site/s, and the privilege level.
+    There is no return value, the new privileges are stored into the global "privilege" variable
+    '''
     role = ""
     while role not in roles:
         role = input("Which level of privilege at the org level (\"s\" for Super Admin, \"n\" for Network Admin,\"o\" for observer,\"h\" for helpdesk)")    
@@ -45,10 +50,13 @@ def define_privileges(org_id):
             privileges.append({"scope": "org", "org_id": org_id, "site_id":site_id, "role":roles[role]})
             break 
 
-def import_admins(org_id):
-    print("Opening CSV file %s" % sys.argv[1])
+def import_admins(file_path, org_id):
+    '''
+    create all the administrators from the "file_path" file.
+    '''
+    print("Opening CSV file %s" % file_path)
     try:
-        with open(sys.argv[1], 'r') as my_file:
+        with open(file_path, 'r') as my_file:
             invite_file = csv.reader(my_file, delimiter=',')
             for row in invite_file:  
                 email= row[0]
@@ -60,14 +68,14 @@ def import_admins(org_id):
         print("Error while opening the CSV file... Aborting")
 
 #### SCRIPT ENTRYPOINT ####
-
+file_path = sys.argv[1]
 mist = mist_lib.Mist_Session("./session.py")
 
 org_id = cli.select_org(mist)
 
 if privileges == []:
     define_privileges(org_id)
-import_admins(org_id)
+import_admins(file_path, org_id)
 
 admins = mist_lib.requests.org.admins.get(mist, org_id)['result']
 print(tabulate(admins))
