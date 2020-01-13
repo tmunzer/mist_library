da0f052a50bfd69669bcd980c84033ea7e1b0cfc
Thomas Munzer <thomas.munzer@gmail.com>
2020-01-13 15:35:20 +0100

    fix function format

--- mlib/requests/org/channels.py
+++ mlib/requests/org/channels.py
@@ -5,8 +5,9 @@ def country_codes_get(mist_session):
     return resp
 
 def ap_channels_get(mist_session, country_code):
-    uri = "/api/v1/const/ap_channels?country_code=%s" % country_code
-    resp = mist_session.mist_get(uri)
+    uri = "/api/v1/const/ap_channels"
+    query = {"country_code":country_code}
+    resp = mist_session.mist_get(uri=uri, query=query)
     return resp
 
 
