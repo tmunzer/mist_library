df975c83355ffd22571189e99260b4f9dacf4c1c
Thomas Munzer <thomas.munzer@gmail.com>
2020-01-16 21:19:20 +0100

    image management added

--- mlib/requests/sites/maps.py
+++ mlib/requests/sites/maps.py
@@ -1,4 +1,3 @@
-
 def get(mist_session, site_id, page=1, limit=100):
     uri = "/api/v1/sites/%s/maps" % site_id
     resp = mist_session.mist_get(uri, site_id=site_id, page=page, limit=limit)
@@ -13,4 +12,16 @@ def create(mist_session, site_id, map_settings):
     uri = "/api/v1/sites/%s/maps" % site_id
     body = map_settings
     resp = mist_session.mist_post(uri, site_id=site_id, body=body)
+    return resp
+
+
+def add_image(mist_session, site_id, map_id, image_path):
+    uri = "/api/v1/sites/%s/maps/%s/image" %(site_id, map_id)
+    files = {'file': open(image_path, 'rb').read()}
+    resp = mist_session.mist_post_file(uri, site_id=site_id, files=files)
+    return resp
+
+def delete_image(mist_session, site_id, map_id):
+    uri = "/api/v1/sites/%s/maps/%s/image" %(site_id, map_id)
+    resp = mist_session.mist_delete(uri, site_id=site_id)
     return resp
