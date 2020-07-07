998c8c753be241cad2209db1811a9b67574101d4
Thomas Munzer <thomas.munzer@gmail.com>
2020-07-07 13:53:26 +0200

    fix typo

--- mlib/requests/orgs/rftemplates.py
+++ mlib/requests/orgs/rftemplates.py
@@ -15,7 +15,7 @@ def get_by_id(mist_session, org_id, rftemplate_id):
     return resp
 
 def update(mist_session, org_id, rftemplate_id, settings):
-    uri = "/api/v1/orgs/%s/rftemprftemplateslate/%s" %(org_id, rftemplate_id)
+    uri = "/api/v1/orgs/%s/rftemplates/%s" %(org_id, rftemplate_id)
     body = settings
     resp = mist_session.mist_put(uri, org_id=org_id, body=body)
     return resp
