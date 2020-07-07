256c680cb8c4c673dce0f9b6b2cbde9ada24f68c
Thomas Munzer <thomas.munzer@gmail.com>
2020-07-07 16:16:30 +0200

    adding get_by_id function

--- mlib/requests/orgs/sitegroups.py
+++ mlib/requests/orgs/sitegroups.py
@@ -6,6 +6,11 @@ def get(mist_session, org_id, page=1, limit=100):
     resp = mist_session.mist_get(uri, org_id, page=page, limit=limit)
     return resp
 
+def get_by_id(mist_session, org_id, sitegroup_id):
+    uri = "/api/v1/orgs/%s/sitegroups/%s" %(org_id, sitegroup_id)
+    resp = mist_session.mist_get(uri, org_id)
+    return resp
+
 def create(mist_session, org_id, group_name):
     uri = "/api/v1/orgs/%s/sitegroups" % org_id
     body = group_name
