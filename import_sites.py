812828a6486f59850a12dda4f81dbfd76929ed6b
Thomas Munzer <thomas.munzer@gmail.com>
2022-03-02 21:18:46 +0100

    add Google API support

--- import_sites.py
+++ import_sites.py
@@ -5,6 +5,8 @@ Github repository: https://github.com/tmunzer/Mist_library/
 
 import mlib as mist_lib
 from mlib import cli
+import requests
+import time
 from geopy import Nominatim
 from tzwhere import tzwhere
 
@@ -16,28 +18,122 @@ finally:
     from mlib.__debug import Console
     console = Console(log_level)
 
+#####################################################################
 #### SETTINGS #####
+# This variables are used to create the sites. All the fields are required
 sites = [
     {
         "name": "test",
-        "address": '41 rue de Villiers, Neuilly sur Seine',
-        "groups": ["test1", "test"]
-    },
-    {
-        "name": "test2",
-        "address": '41 rue de Villiers, Neuilly sur Seine',
-        "groups": ["test2", "test"]
+        "address": '41 rue de Villiers, Neuilly sur Seine, France',
+        "groups": ["test1", "test2"]
     }
 ]
-
-
-#### GLOBALS #####
-
-geolocator = Nominatim(user_agent="import_app")
-tzwhere = tzwhere.tzwhere()
+# org_id is optional. If not configured, the script will let you select an org
 org_id = ""
 
-#### FUNCTIONS #####
+# This Script can use Google APIs (optional) to retrieve lat/lng, tz and country code. To be 
+# able to use Google API, you need an API Key first. Mode information available here:
+# https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+google_api_key = ""
+
+#####################################################################
+#### GLOBALS #####
+geolocator = Nominatim(user_agent="import_app")
+tzwhere = tzwhere.tzwhere()
+
+#####################################################################
+# Open Geocoding
+#####################################################################
+
+
+def get_open_geocoding(address):
+    console.notice(">> Retrieving lat/lng from OpenStreetMap API")
+    location = geolocator.geocode(address, addressdetails=True)
+    if type(location) == "NoneType":
+        console.error("Unable to find the address %s" % address)
+    else:
+        return location
+
+
+def get_open_tz(location):
+    console.notice(">> Retrieving tz and country code with tzwhere")
+    tz = tzwhere.tzNameAt(location.latitude, location.longitude)
+    country_code = str(location.raw["address"]["country_code"]).upper()
+    return {"tz": tz, "country_code": country_code}
+
+
+def use_open(address):
+    data = {}
+    location = get_open_geocoding(address)
+    data = {
+        "location": {
+            "latitude": location.latitude,
+            "longitude": location.longitude
+        },
+        **get_open_tz(location)
+    }
+    return data
+
+#####################################################################
+# Google Geocoding
+#####################################################################
+
+
+def get_google_geocoding(address):
+    console.notice(">> Retrieving lat/lng and country code from Google API")
+    data = {"location": None, "country_code": ""}
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}".format(
+        address, google_api_key)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data["status"] == "OK":
+            if len(data["results"]) > 0:
+                data["location"] = {
+                    "address": data["results"][0]["formatted_address"],
+                    "latitude": data["results"][0]["geometry"]["location"]["lat"],
+                    "longitude": data["results"][0]["geometry"]["location"]["lng"]
+                }
+                for entry in data["results"][0]["address_components"]:
+                    if "country" in entry["types"]:
+                        data["country_code"] = entry["short_name"]
+                return data
+        elif data["status"] == "REQUEST_DENIED":
+            console.error(data["error_message"])
+            return data
+    else:
+        console.error("Unable to get the location from Google API")
+        return data
+
+
+def get_google_tz(location):
+    console.notice(">> Retrieving tz from Google API")
+    ts = int(time.time())
+    url = "https://maps.googleapis.com/maps/api/timezone/json?location={0},{1}&timestamp={2}&key={3}".format(
+        location["latitude"], location["longitude"], ts, google_api_key)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data["status"] == "OK":
+            tz = data["timeZoneId"]
+            return tz
+        elif data["status"] == "REQUEST_DENIED":
+            console.error(data["error_message"])
+    else:
+        console.error("Unable to get the timezone from Google API")
+
+
+def use_google(address):
+    data = get_google_geocoding(address)
+    if data["location"] != None:
+        data["tz"] = get_google_tz(data["location"])
+        return data
+    else:
+        return None
+
+#####################################################################
+# Site Groups Management
+#####################################################################
 
 
 def get_site_groups_list():
@@ -53,12 +149,13 @@ def get_site_groups_list():
 
 
 def create_site_group(group_name):
-    response = mist_lib.orgs.sitegroups.create(mist, org_id, {"name":group_name})
+    response = mist_lib.orgs.sitegroups.create(
+        mist, org_id, {"name": group_name})
     if response['status_code'] == 200:
         name = response["result"]["name"]
         sitegroups_id = response["result"]["id"]
-        console.notice("Site Group %s created with ID %s" %
-                       (name, sitegroups_id))
+        console.notice(
+            "> Site Group {0} created with ID {1}".format(name, sitegroups_id))
         return sitegroups_id
 
 
@@ -74,48 +171,76 @@ def assign_site_to_group(site_id, group_name):
     response = mist_lib.orgs.sitegroups.update(
         mist, org_id, group_id, {"site_ids": site_ids})
     if response["status_code"] == 200:
-        console.notice("Site succesfully added to group %s (id %s)" %
-                       (group_name, group_id))
+        console.info("> Site succesfully added to group {0} (group_id {1})".format(
+            group_name, group_id))
         return group_id
+    else:
+        console.warning("> Unable to add the site to group {0} (group_id {1})".format(
+            group_name, group_id))
+        return None
 
 
-def assign_groups_to_site(site_id, site_name, group_ids):
+def assign_groups_to_site(site_id, group_ids):
     response = mist_lib.orgs.sites.update(
         mist, org_id, site_id, {"sitegroup_ids": group_ids})
     if response["status_code"] == 200:
-        console.notice("Groups succesfully added to site %s (id %s)" %
-                       (site_name, site_id))
+        console.info("> Groups assigned to the site")
         return response
+    else:
+        console.warning("> Unable to assign groups")
+        return None
+
+#####################################################################
+# Site  Management
+#####################################################################
 
 
 def create_site(name, address):
-    location = geolocator.geocode(address, addressdetails=True)
-    if type(location) == "NoneType":
-        console.error("Unable to find the address %s" % address)
+    console.info("> Retrieving additional data")
+    if google_api_key:
+        data = use_google(site["address"])
+    if not google_api_key or data == None:
+        data = use_open(site["address"])
+        payload = {
+            "name": name,
+            "address": address,
+            "latlng": {
+                "lat": data["location"]["latitude"],
+                "lng": data["location"]["longitude"]
+            },
+            "timezone": data["tz"],
+            "country_code": data["country_code"]
+        }
+    response = mist_lib.orgs.sites.create(mist, org_id, payload)
+    if response["status_code"] == 200:
+        console.info("> Site created succesfully with ID {0}".format(
+            response["result"]["id"]))
+        return response["result"]
     else:
-        console.info("Address found: %s" % location)
-        tz = tzwhere.tzNameAt(location.latitude, location.longitude)
-        country_code = str(location.raw["address"]["country_code"]).upper()
-        response = mist_lib.orgs.sites.create(mist, org_id, {"name":name, "address":location.address,
-                                              "latlng": {"lat":location.latitude, "lng":location.longitude}, "timezone":tz, "country_code":country_code})
-        if response["status_code"] == 200:
-            console.notice("Site %s created succesfully with ID %s" %
-                           (name, response["result"]["id"]))
-            return response["result"]
+        console.error("> Unable to create site with the payload: {0}".format(payload))
+
 
 
 def new_site(name, address, groups):
+    console.info("Site {0}: Starting process".format(name))
     site = create_site(name, address)
     group_ids = []
+    console.notice("> Retrieving site group ids")
     for group in groups:
-        group_ids.append(assign_site_to_group(site["id"], group))
-    assign_groups_to_site(site["id"], name, group_ids)
+        group_id = assign_site_to_group(site["id"], group)
+        if group_id:
+            group_ids.append(group_id)
+    assign_groups_to_site(site["id"], group_ids)
 
 
+#####################################################################
 ##### ENTRY POINT #####
+#####################################################################
 if __name__ == "__main__":
     mist = mist_lib.Mist_Session("./session.py")
     # mist.save()
-    org_id=cli.select_org(mist)[0]
+    if not org_id:
+        org_id = cli.select_org(mist)[0]
     for site in sites:
-        new_site(name=site["name"], address=site["address"], groups=site["groups"])
+        new_site(name=site["name"], address=site["address"],
+                 groups=site["groups"])
