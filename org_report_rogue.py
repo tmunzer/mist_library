89844134b996f0c0159596fc08fdb5ea58b896e9
Thomas Munzer <thomas.munzer@gmail.com>
2022-09-14 10:02:27 +0200

    improve rogue reporting

--- org_report_rogue.py
+++ org_report_rogue.py
@@ -21,40 +21,52 @@ r_types = [ "honeypot", "lan", "others", "spoof"]
 #### IMPORTS #####
 import mlib as mist_lib
 from mlib import cli
-mist = mist_lib.Mist_Session("./session.py")
 
 #### GLOBAL VARIABLES ####
 rogues_summarized = []
 
 #### FUNCTIONS ####
+def get_rogues(org_info, site_ids):
+    for site_id in site_ids:
+        for r_type in r_types:
+            print(f"{org_info['name']} > {site_id} > {r_type} ".ljust(79, "."), end='', flush=True)
+            rogues = []
+            site_info = mist_lib.requests.sites.info.get(mist, site_id)["result"]
+            site_rogues = mist_lib.requests.sites.rogues.report(mist, site_id, r_type, fields)
+            for rogue in site_rogues:    
+                rogue.insert(0, org_info["name"])
+                rogue.insert(1, org_info["id"])
+                rogue.insert(2, site_info["name"])
+                rogue.insert(3, site_info["id"])
+                rogue.insert(4, r_type)
+                rogues_summarized.append(rogue)
+            print("\033[92m\u2714\033[0m")
+
+
 
 #### SCRIPT ENTRYPOINT ####
-for r_type in r_types:
-    for entry in mist.privileges:    
-        if not "site_id" in entry and "org_id" in entry and entry["org_id"] != "":
-            sites = mist_lib.requests.orgs.sites.get(mist, entry["org_id"])["result"]
-            for site in sites:
-                rogues = []
-                site_rogues = mist_lib.requests.sites.rogues.report(mist, site["id"], r_type, fields)
-                for rogue in site_rogues:    
-                    rogue.insert(0, entry["name"])
-                    rogue.insert(1, entry["org_id"])
-                    rogue.insert(2, site["name"])
-                    rogue.insert(3, site["id"])
-                    rogue.insert(4, r_type)
-                    rogues_summarized.append(rogue)
+mist = mist_lib.Mist_Session()
 
+org_id = cli.select_org(mist, allow_many=False)
+if len(org_id) == 0:
+    print("No Org selected... Exiting...")
+else:
+    site_ids = cli.select_site(mist, org_id[0], allow_many=True)
+    if len(site_ids) <= 0:
+        print("No Site selected... Exiting...")
+    else:
+        print(" Process Started ".center(80, '-'))
+        org_info = mist_lib.requests.orgs.info.get(mist, org_id[0])["result"]
+        get_rogues(org_info, site_ids)
+                    
+        fields.insert(0, "org_name")
+        fields.insert(1, "org_id")
+        fields.insert(2, "site_name")
+        fields.insert(3, "site_id")
+        fields.insert(4, "type")
 
-
-
-            
-fields.insert(0, "org_name")
-fields.insert(1, "org_id")
-fields.insert(2, "site_name")
-fields.insert(3, "site_id")
-fields.insert(4, "type")
-
-cli.show(rogues_summarized, fields)
-cli.save_to_csv(csv_file, rogues_summarized, fields, csv_separator)
+        print(" Process Done ".center(80, '-'))
+        cli.show(rogues_summarized, fields)
+        cli.save_to_csv(csv_file, rogues_summarized, fields, csv_separator)
 
 
