b111811d5c06d2050103f7059450b295e28918fe
Thomas Munzer <thomas.munzer@gmail.com>
2023-02-15 14:19:18 +0100

    moving to mistapi

--- scripts/sites/site_conf_backup.py
+++ scripts/sites/site_conf_backup.py
@@ -1,55 +1,146 @@
 '''
-Written by Thomas Munzer (tmunzer@juniper.net)
-Github repository: https://github.com/tmunzer/Mist_library/
+-------------------------------------------------------------------------------
 
-Python script to backup a whole site to file/s.
-You can use the script "site_conf_restore.py" to restore the generated backup file to an
-existing organization (the organization can be empty, but it must exist).
+    Written by Thomas Munzer (tmunzer@juniper.net)
+    Github repository: https://github.com/tmunzer/Mist_library/
+
+    This script is licensed under the MIT License.
+
+-------------------------------------------------------------------------------
+Python script to backup a whole site.
+You can use the script "site_conf_deploy.py" to restore the generated backup files to an
+existing organization (empty or not) or to a new one.
 
 This script will not change/create/delete/touch any existing objects. It will just
-get every single object from the organization, and save it into a file
+retrieve every single object from the organization.
 
-You can configure some parameters at the beginning of the script if you want
-to change the default settings.
-You can run the script with the command "python3 site_conf_backup.py"
+-------
+Requirements:
+mistapi: https://pypi.org/project/mistapi/
 
-The script has 3 different steps:
-1) admin login
-2) choose the  org
-3) choose the site or sites to backup
-3) backup all the objects to the json file. 
+-------
+Usage:
+This script can be run as is (without parameters), or with the options below.
+If no options are defined, or if options are missing, the missing options will
+be asked by the script or the default values will be used.
+
+It is recomended to use an environment file to store the required information
+to request the Mist Cloud (see https://pypi.org/project/mistapi/ for more 
+information about the available parameters).
+
+-------
+Script Parameters:
+-h, --help              display this help
+-o, --org_id=           Set the org_id (required to backup templates assigned to the site)
+-s, --site_id=          Set the site_id. If the provided site_id does not belong to the 
+                        provided org_id or if the site_id is not providced, the script will
+                        propose to select the site to backup
+-b, --backup_folder=    Path to the folder where to save the org backup (a subfolder
+                        will be created with the org name)
+                        default is "./org_backup"
+-l, --log_file=         define the filepath/filename where to write the logs
+                        default is "./script.log"
+-e, --env=              define the env file to use (see mistapi env file documentation 
+                        here: https://pypi.org/project/mistapi/)
+                        default is "~/.mist_env"
+
+-------
+Examples:
+python3 ./site_conf_backup.py
+python3 ./site_conf_backup.py --org_id=203d3d02-xxxx-xxxx-xxxx-76896a3330f4 -s a39d0e91-xxxx-xxxx-xxxx-42df868c5a0b
+
 '''
 
 #### IMPORTS ####
-import mistapi
-from mistapi.__logger import console
-import os
-import urllib.request
-import json
 import logging
+import json
+import urllib.request
+import os
+import sys
+import getopt
 
+try:
+    import mistapi
+    from mistapi.__logger import console
+except:
+    print("""
+Critical: 
+\"mistapi\" package is missing. Please use the pip command to install it.
+
+# Linux/macOS
+python3 -m pip install mistapi
+
+# Windows
+py -m pip install mistapi
+    """)
+    sys.exit(2)
+
+#####################################################################
 #### PARAMETERS #####
-backup_root_folder = "site_backup"
-backup_file = "./site_conf_file.json"
+backup_folder = "./site_backup"
+backup_file = "site_conf_file.json"
+log_file = "./script.log"
 file_prefix = ".".join(backup_file.split(".")[:-1])
-log_file = "./site_conf_backup.log"
-env_file = "./.env"
+env_file = "~/.mist_env"
+
+#####################################################################
 #### LOGS ####
 logger = logging.getLogger(__name__)
 
-#### FUNCTIONS ####
 
-def _backup_obj(m_func, obj_type):
-    print(f"Backuping {obj_type} ".ljust(79, "."), end="", flush=True)
-    try: 
-        res = m_func.data
-        print('\033[92m\u2714\033[0m')
-    except:
-        res = None
-        print('\033[31m\u2716\033[0m')
-    finally:
-        return res
+#####################################################################
+# BACKUP OBJECTS REFS
+org_steps = {
+    "alarmtemplate": {"mistapi_function": mistapi.api.v1.orgs.alarmtemplates.getOrgAlarmTemplates, "text": "Org alarmtemplates"},
+    "rftemplate": {"mistapi_function": mistapi.api.v1.orgs.rftemplates.getOrgRfTemplates, "text": "Org rftemplates"},
+    "networktemplate": {"mistapi_function": mistapi.api.v1.orgs.networktemplates.getOrgNetworkTemplates, "text": "Org networktemplates"},
+    "secpolicy": {"mistapi_function": mistapi.api.v1.orgs.secpolicies.getOrgSecPolicies, "text": "Org secpolicies"},
+}
+site_steps = {        
+    "info": {"mistapi_function": mistapi.api.v1.sites.sites.getSiteInfo, "text": "Site info"},
+    "settings": {"mistapi_function": mistapi.api.v1.sites.setting.getSiteSetting, "text": "Site settings"},
+    "assets": {"mistapi_function": mistapi.api.v1.sites.assets.getSiteAssets, "text": "Site assets"},
+    "assetfilters": {"mistapi_function": mistapi.api.v1.sites.assetfilters.getSiteAssetFilters, "text": "Site assetfilters"},
+    "beacons": {"mistapi_function": mistapi.api.v1.sites.beacons.getSiteBeacons, "text": "Site beacons"},
+    "maps": {"mistapi_function": mistapi.api.v1.sites.maps.getSiteMaps, "text": "Site maps"},
+    "psks": {"mistapi_function": mistapi.api.v1.sites.psks.getSitePsks, "text": "Site psks"},
+    "rssizones": {"mistapi_function": mistapi.api.v1.sites.rssizones.getSiteRssiZones, "text": "Site rssizones"},
+    "vbeacons": {"mistapi_function": mistapi.api.v1.sites.vbeacons.getSiteVBeacons, "text": "Site vbeacons"},
+    "webhooks": {"mistapi_function": mistapi.api.v1.sites.webhooks.getSiteWebhooks, "text": "Site webhooks"},
+    "wlans": {"mistapi_function": mistapi.api.v1.sites.wlans.getSiteWlans, "text": "Site wlans"},
+    "wxrules": {"mistapi_function": mistapi.api.v1.sites.wxrules.getSiteWxRules, "text": "Site wxrules"},
+    "wxtags": {"mistapi_function": mistapi.api.v1.sites.wxtags.getSiteWxTags, "text": "Site wxtags"},
+    "wxtunnels": {"mistapi_function": mistapi.api.v1.sites.wxtunnels.getSiteWxTunnels, "text": "Site wxtunnels"},
+    "zones": {"mistapi_function": mistapi.api.v1.sites.zones.getSiteZones, "text": "Site zones"}
+}
 
+#####################################################################
+#### BACKUP FUNCTIONS ####
+def log_message(message):
+    print(f"{message}".ljust(79, '.'), end="", flush=True)
+
+def log_success(message):
+    print("\033[92m\u2714\033[0m")
+    logger.info(f"{message}: Success")
+
+def log_failure(message):
+    print('\033[31m\u2716\033[0m')
+    logger.exception(f"{message}: Failure")    
+
+def _do_backup(mist_session, backup_function, scope_id, message, request_type:str=None):
+    try:
+        log_message(message)
+        if request_type:
+            response = backup_function(mist_session, scope_id, type=request_type)
+        else:
+            response = backup_function(mist_session, scope_id)
+        data = mistapi.get_all(mist_session, response)
+        log_success(message)
+        return data
+    except Exception as e:
+        log_failure(message)
+        logger.error("Exception occurred", exc_info=True)
+        return None
 
 def _backup_wlan_portal(org_id, site_id, wlans):  
     for wlan in wlans:     
@@ -76,6 +167,8 @@ def _backup_wlan_portal(org_id, site_id, wlans):
                 print('\033[31m\u2716\033[0m')
     
 
+#####################################################################
+#### BACKUP SITE ####
 def _backup_site(apisession, site_id, site_name, org_id):
     print()
     console.info(f"Backup: processing site {site_name} ...")
@@ -105,49 +198,17 @@ def _backup_site(apisession, site_id, site_name, org_id):
         "sitegroup_names": []   
     } 
 
-    site_backup["site"]["settings"] = _backup_obj(mistapi.api.v1.sites.setting.getSiteSetting(apisession, site_id), "site settings")
 
-    site_backup["site"]["info"] = _backup_obj(mistapi.api.v1.sites.sites.getSiteInfo(apisession, site_id), "site info")
+    for step_name in site_steps:
+                step = site_steps[step_name]
+                site_backup["site"][step_name] = _do_backup(apisession, step["mistapi_function"], site_id, step["text"])
 
-    site_backup["site"]["assets"] = _backup_obj(mistapi.api.v1.sites.assets.getSiteAssets(apisession, site_id), "assets")
-    
-    site_backup["site"]["assetfilters"] = _backup_obj(mistapi.api.v1.sites.assetfilters.getSiteAssetFilters(apisession, site_id), "assetfilters")
-
-    site_backup["site"]["beacons"] = _backup_obj(mistapi.api.v1.sites.beacons.getSiteBeacons(apisession, site_id), "beacons")
-
-    site_backup["site"]["maps"] = _backup_obj(mistapi.api.v1.sites.maps.getSiteMaps(apisession, site_id), "maps")
-
-    site_backup["site"]["psks"] = _backup_obj(mistapi.api.v1.sites.psks.getSitePsks(apisession, site_id), "psks")
-
-    site_backup["site"]["rssizones"] = _backup_obj(mistapi.api.v1.sites.rssizones.getSiteRssiZones(apisession, site_id), "rssizones")
-
-    site_backup["site"]["vbeacons"] = _backup_obj(mistapi.api.v1.sites.vbeacons.getSiteVBeacons(apisession, site_id), "vbeacons")
-
-    site_backup["site"]["webhooks"] = _backup_obj(mistapi.api.v1.sites.webhooks.getSiteWebhooks(apisession, site_id), "webhooks")
-
-    site_backup["site"]["wlans"] = _backup_obj(mistapi.api.v1.sites.wlans.getSiteWlans(apisession, site_id), "wlans")
 
     _backup_wlan_portal(org_id, site_id, site_backup["site"]["wlans"])
 
-    site_backup["site"]["wxrules"] = _backup_obj(mistapi.api.v1.sites.wxrules.getSiteWxRules(apisession, site_id), "wxrules")
-
-    site_backup["site"]["wxtags"] = _backup_obj(mistapi.api.v1.sites.wxtags.getSiteWxTags(apisession, site_id), "wxtags")
-
-    site_backup["site"]["wxtunnels"] = _backup_obj(mistapi.api.v1.sites.wxtunnels.getSiteWxTunnels(apisession, site_id), "wxtunnels")
-
-    site_backup["site"]["zones"] = _backup_obj(mistapi.api.v1.sites.zones.getSiteZones(apisession, site_id), "zones")
-
-    if "rftemplate_id" in site_backup["site"]["info"] and site_backup["site"]["info"]["rftemplate_id"]:
-        site_backup["rftemplate"] = _backup_obj(mistapi.api.v1.orgs.rftemplates.getOrgRfTemplate(apisession, org_id, site_backup["site"]["info"]["rftemplate_id"]), "RF Template")
-        
-    if "secpolicy_id" in site_backup["site"]["info"] and site_backup["site"]["info"]["secpolicy_id"]:
-        site_backup["secpolicy"] = _backup_obj(mistapi.api.v1.orgs.secpolicies.getOrgSecPolicy(apisession, org_id, site_backup["site"]["info"]["secpolicy_id"]), "Security Policy")
-
-    if "alarmtemplate_id" in site_backup["site"]["info"] and site_backup["site"]["info"]["alarmtemplate_id"]:
-        site_backup["alarmtemplate"] = _backup_obj(mistapi.api.v1.orgs.alarmtemplates.getOrgAlarmTemplate(apisession, org_id, site_backup["site"]["info"]["alarmtemplate_id"]), "Alarm Template")
-
-    if "networktemplate_id" in site_backup["site"]["info"] and site_backup["site"]["info"]["networktemplate_id"]:
-        site_backup["networktemplate"] = _backup_obj(mistapi.api.v1.orgs.networktemplates.getOrgNetworkTemplate(apisession, org_id, site_backup["site"]["info"]["networktemplate_id"]), "Network Tempalte")
+    for step_name in org_steps:
+        if f"{step_name}_id" in site_backup["site"]["info"] and site_backup["site"]["info"][f"{step_name}_id"]:
+            site_backup[step_name] = _do_backup(apisession, step["mistapi_function"], org_id, step["text"])
 
     if "sitegroup_ids" in site_backup["site"]["info"] and site_backup["site"]["info"]["sitegroup_ids"]:
         for sitegroup_id in site_backup["site"]["info"]["sitegroup_ids"]:
@@ -168,6 +229,8 @@ def _backup_site(apisession, site_id, site_name, org_id):
 
     return site_backup
 
+#####################################################################
+#### SAVING FUNCTIONS ####
 def _save_to_file(backup_file, backup):
     print(f"Saving backup to {backup_file} file...".ljust(79, "."), end="", flush=True)
     try:
@@ -185,7 +248,7 @@ def _goto_folder(folder_name):
     
 
 def start_site_backup(apisession, org_id, org_name, site_ids):
-    _goto_folder(backup_root_folder)
+    _goto_folder(backup_folder)
     _goto_folder(org_name)
     
     for site_id in site_ids:
@@ -202,20 +265,119 @@ def start_site_backup(apisession, org_id, org_name, site_ids):
 
 
 
-def start(apisession):
-    org_id = mistapi.cli.select_org(apisession)[0]
+def start(apisession:mistapi.APISession, org_id:str, site_id:str, backup_folder_param:str):
+    if not org_id: org_id = mistapi.cli.select_org(apisession)[0]
+    if not site_id: apisession = mistapi.cli.select_site(apisession)[0]
+    else:
+        response = mistapi.api.v1.orgs.sites.getOrgSites(apisession, org_id)
+        org_sites = mistapi.get_all(response)
+        site_id_in_org = False
+        for site in org_sites:
+            if site["id"] == site_id:
+                site_id_in_org = True
+                break
+        if not site_id_in_org:
+            console.error(f"Site ID {site_id} does not belong to org {org_id}. Please select another site.")
+            start(apisession, org_id, None, backup_folder_param)
+
+    current_folder = os.getcwd()
+    if backup_folder_param:
+        global backup_folder 
+        backup_folder = backup_folder_param
     org_name = mistapi.api.v1.orgs.orgs.getOrgInfo(apisession, org_id=org_id).data["name"]
     site_id = mistapi.cli.select_site(apisession, org_id=org_id, allow_many=True)
     start_site_backup(apisession, org_id, org_name, site_id)
+    os.chdir(current_folder)
 
 
+#####################################################################
+##### USAGE ####
+def usage():
+    print('''
+-------------------------------------------------------------------------------
+
+    Written by Thomas Munzer (tmunzer@juniper.net)
+    Github repository: https://github.com/tmunzer/Mist_library/
+
+    This script is licensed under the MIT License.
+
+-------------------------------------------------------------------------------
+Python script to backup a whole site.
+You can use the script "site_conf_deploy.py" to restore the generated backup files to an
+existing organization (empty or not) or to a new one.
+
+This script will not change/create/delete/touch any existing objects. It will just
+retrieve every single object from the organization.
+
+-------
+Requirements:
+mistapi: https://pypi.org/project/mistapi/
+
+-------
+Usage:
+This script can be run as is (without parameters), or with the options below.
+If no options are defined, or if options are missing, the missing options will
+be asked by the script or the default values will be used.
+
+It is recomended to use an environment file to store the required information
+to request the Mist Cloud (see https://pypi.org/project/mistapi/ for more 
+information about the available parameters).
+
+-------
+Script Parameters:
+-h, --help              display this help
+-o, --org_id=           Set the org_id (required to backup templates assigned to the site)
+-s, --site_id=          Set the site_id. If the provided site_id does not belong to the 
+                        provided org_id or if the site_id is not providced, the script will
+                        propose to select the site to backup
+-b, --backup_folder=    Path to the folder where to save the org backup (a subfolder
+                        will be created with the org name)
+                        default is "./org_backup"
+-l, --log_file=         define the filepath/filename where to write the logs
+                        default is "./script.log"
+-e, --env=              define the env file to use (see mistapi env file documentation 
+                        here: https://pypi.org/project/mistapi/)
+                        default is "~/.mist_env"
+
+-------
+Examples:
+python3 ./site_conf_backup.py
+python3 ./site_conf_backup.py --org_id=203d3d02-xxxx-xxxx-xxxx-76896a3330f4 -s a39d0e91-xxxx-xxxx-xxxx-42df868c5a0b
+
+''')
+#####################################################################
 ##### ENTRY POINT ####
-
 if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:s:e:l:b:", [
+                                   "help", "org_id=", "site_id=", "env=", "log_file=", "backup_folder="])
+    except getopt.GetoptError as err:
+        console.error(err)
+        usage()
+
+    org_id = None
+    site_id = None
+    backup_folder_param = None
+    for o, a in opts:
+        if o in ["-h", "--help"]:
+            usage()
+        elif o in ["-o", "--org_id"]:
+            org_id = a      
+        elif o in ["-s", "--site_id"]:
+            org_id = a      
+        elif o in ["-e", "--env"]:
+            env_file = a
+        elif o in ["-l", "--log_file"]:
+            log_file = a
+        elif o in ["-b", "--backup_folder"]:
+            backup_folder_param = a
+        else:
+            assert False, "unhandled option"
+
     #### LOGS ####
     logging.basicConfig(filename=log_file, filemode='w')
     logger.setLevel(logging.DEBUG)
     ### START ###
     apisession = mistapi.APISession(env_file=env_file)
     apisession.login()
-    start(apisession)
+    start(apisession, org_id, site_id, backup_file)
